67e596184c5720995b7c0ef14990eb9fcb1b7702
Tadesse-Alemayehu <email24t@gmail.com>
2022-03-20 11:48:02 +0300

    Import modules

---
+++ index.js
@@ -0,0 +1,4 @@
+import { } from './modules/attributes-module.js';
+import { } from './modules/localStorage-module.js';
+import { } from './modules/methods-module.js';
+import { } from './modules/MyBook-module.js';
